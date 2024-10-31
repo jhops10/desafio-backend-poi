e5ada088dc86f39384e1ae64d9139cf62635cb35
João Paulo <joaowais@gmail.com>
2024-10-31 20:09:27 -0300

    Adiciona método para calcular distancia

--- src/main/java/com/jhops10/poi/controller/PointOfInterestController.java
+++ src/main/java/com/jhops10/poi/controller/PointOfInterestController.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class PointOfInterestController {
 
@@ -32,5 +34,28 @@ public class PointOfInterestController {
         var body = repository.findAll(PageRequest.of(page, pageSize));
 
         return ResponseEntity.ok(body);
+    }
+
+    @GetMapping("/near-me")
+    public ResponseEntity<List<PointOfInterest>> nearMe(@RequestParam("x") Long x,
+                                                        @RequestParam("y") Long y,
+                                                        @RequestParam("dmax") Long dmax) {
+
+        var xMin = x - dmax;
+        var xMax = x + dmax;
+        var yMin = y - dmax;
+        var yMax = y + dmax;
+
+
+        var body = repository.findNearMe(xMin, xMax, yMin,yMax)
+                .stream()
+                .filter(p -> distanceBetweenPoints(x, y, p.getX(), p.getY()) <= dmax)
+                .toList();
+
+        return ResponseEntity.ok(body);
+    }
+
+    private Double distanceBetweenPoints(Long x1, Long y1, Long x2, Long y2) {
+        return Math.sqrt((Math.pow(x2 - x1, 2) + (Math.pow(y2 - y1, 2))));
     }
 }
